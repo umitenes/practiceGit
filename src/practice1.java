7aa426894d9a354e269c5542f28b7ee9df6b1fe3
umitakdag <umitakdag@gmail.com>
2025-11-16 18:22:28 +0200

    added more codes

--- src/practice1.java
+++ src/practice1.java
@@ -2,6 +2,7 @@ public class practice1 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Welcome to Java club");
+        System.out.println("Have fun with it");
     }
 
 }
